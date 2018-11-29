7aedd54c0606b7144dbb6f4c6675eba4420fc364
zhaokh <wwwzhaokh@126.com>
2018-11-29 16:46:30 +0800

    v1.3 引入json文件

--- app/Greeter.js
+++ app/Greeter.js
@@ -1,6 +1,8 @@
 // Greeter.js
+var config = require('./config.json');
+
 module.exports = function() {
-    var greet = document.createElement('div');
-    greet.textContent = "Hi there and greetings!";
-    return greet;
-  };
+  var greet = document.createElement('div');
+  greet.textContent = config.greetText;
+  return greet;
+};
