a5661124ebec79e5eafa79a81a75f3883cc8b8af
zhaokh <wwwzhaokh@126.com>
2018-11-29 17:08:37 +0800

    v1.4 出现babel/core的兼容性问题，报错

--- app/Greeter.js
+++ app/Greeter.js
@@ -1,8 +1,15 @@
-// Greeter.js
-var config = require('./config.json');
+//Greeter,js
+import React, {Component} from 'react'
+import config from './config.json';
 
-module.exports = function() {
-  var greet = document.createElement('div');
-  greet.textContent = config.greetText;
-  return greet;
-};
+class Greeter extends Component{
+  render() {
+    return (
+      <div>
+        {config.greetText}
+      </div>
+    );
+  }
+}
+
+export default Greeter
